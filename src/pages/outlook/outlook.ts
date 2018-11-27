2da0fc4c37509579593fcad8e49cebd597581583
scooterpooter24 <scoott@vt.edu>
2018-11-27 00:03:12 -0500

    Fixed bug with repeted entry and messed with chart.js

--- src/pages/outlook/outlook.ts
+++ src/pages/outlook/outlook.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Chart } from 'chart.js';
-import 'node-finance';
 import { Loan } from './loan';
 
 @Component({
@@ -12,6 +11,7 @@ export class OutlookPage {
 
   @ViewChild('pieCanvas') pieCanvas;
   @ViewChild('debtCanvas') debtCanvas;
+  
 
   loan = new Loan();
   total: number;
@@ -31,6 +31,9 @@ export class OutlookPage {
   }
 
   NPER(loan) {
+    this.numPeriods = 0;
+    this.totalInterestPaid = 0;
+    this.total = 0;
     var interestMonthly = loan.interestRate / 12;
     var moneyLeft = loan.principle;
     var monthlyPaymentCollege = this.paymentCollege;
@@ -41,21 +44,16 @@ export class OutlookPage {
         if(collegePeriods > 0){
             this.totalInterestPaid += moneyLeft*(interestMonthly/100);
             moneyLeft = (moneyLeft *(1+interestMonthly/100)) - monthlyPaymentCollege;
-            console.log(moneyLeft);
-            console.log(collegePeriods)
             collegePeriods -= 1;
-            if (this.numPeriods % 12 === 0){
-                debtHistory.push(-moneyLeft)
+            if (this.numPeriods % 12 === 0) {
+                debtHistory.push(-moneyLeft);
             }
 
         } else {
-            console.log("Out of College")
-            console.log(monthlyPaymentPostCollege)
             this.totalInterestPaid += moneyLeft*(interestMonthly/100);
             moneyLeft = (moneyLeft *(1+interestMonthly/100)) - monthlyPaymentPostCollege;
-            if (this.numPeriods % 12 === 0){
-                console.log("Added"); 
-                debtHistory.push(-moneyLeft)
+            if (this.numPeriods % 12 === 0) {
+                debtHistory.push(-moneyLeft);
             }
         } 
         
@@ -69,25 +67,23 @@ export class OutlookPage {
   }
 
   showChart() {
+    
     this.NPER(this.loan);
     this.totalNumPeriods = Math.round(this.numPeriods / 12); 
     var i;
     var today = new Date(); 
     var todayYear = today.getFullYear();
-    var yearsArray = []
+    var yearsArray = [];
     for (i = 0; i <= (this.numPeriods / 12); i++ ){
         yearsArray.push(todayYear + i);
-        console.log(yearsArray); 
     }
     this.total = Math.round(+this.loan.principle + +this.totalInterestPaid);
     this.activeChart = true;
     this.totalInterestPaid = Math.round(this.totalInterestPaid)
     var ip = this.totalInterestPaid;
     var p = this.loan.principle;
-    console.log(this.debtHistory);
-
+    Chart.defaults.scale.gridLines.display = false;
     this.pieChart = new Chart(this.pieCanvas.nativeElement, {
- 
       type: 'pie',
       data: { 
           labels: ["Interest", "Principle"],
@@ -104,11 +100,9 @@ export class OutlookPage {
               ]
           }]
       }
-
     });
 
     this.debtChart = new Chart(this.debtCanvas.nativeElement, {
- 
         type: 'line',
         data: {
             labels: yearsArray, 
